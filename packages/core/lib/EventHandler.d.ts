e8743d2d1a1995e696b570a336ebf5238b8bf4ae
liulu <1021283712@qq.com>
2023-04-04 22:49:13 +0800

    build: 删除构建的产物

--- packages/core/lib/EventHandler.d.ts
+++
@@ -1,9 +0,0 @@
-declare class EventHandler {
-    handlers: Record<string, Function[]>;
-    constructor(handlers?: {});
-    addHandler(ev: string, handler: Function): void;
-    removeHandler(ev: string, handler: Function): void;
-    handle(ev: string, once?: boolean, ...args: unknown[]): void;
-    getHandler(ev: string): Function[];
-}
-export default EventHandler;
